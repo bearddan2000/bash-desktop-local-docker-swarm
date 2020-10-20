470c6fcfd89dd8c1e6513c82a35e44ac9a2656b9
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-10-20 10:33:08 -0400

    Add files via upload
    rename repo
    added more verbose script

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# docker-local-swarm
-Implements a docker cluster locally
+# bash-desktop-local-docker-swarm
+
+## Description
+This will create a local docker swarm with 6 nodes, 3 masters and 3 slaves. Since this creates 6 virtual machines,
+a lot of memory and resources such as file swapping need to be availble. The easiest way to start this is to have
+virtual box already installed, the script doesn't check.
+
+## Tech stack
+- bash
+
+## Docker stack
+- docker-cli
+- docker-machine
+- docker-swarm
+
+## Prerequisites
+- at least 20GB of RAM
+- docker installed
+- virtual box drivers
+
+## To run
+```sudo ./install.sh -u```
+
+## To stop
+```sudo ./install.sh -d```
+
+## To verify
+Run the command `docker info` as root.
+
+## Credit
+https://linuxbsdos.com/2016/12/14/how-to-install-docker-machine-on-linux-mint-18-and-18-1/
+https://mmorejon.io/en/blog/docker-swarm-with-docker-machine-scripts/
+https://docs.docker.com/engine/swarm/swarm-tutorial/create-swarm/
--- install.sh
+++ install.sh
@@ -1,5 +1,133 @@
-#!/bin/bash
+#!/bin/env bash
 
-# https://linuxbsdos.com/2016/12/14/how-to-install-docker-machine-on-linux-mint-18-and-18-1/
-# https://mmorejon.io/en/blog/docker-swarm-with-docker-machine-scripts/
-# https://docs.docker.com/engine/swarm/swarm-tutorial/create-swarm/
+basefile="install"
+logfile="general.log"
+timestamp=`date '+%Y-%m-%d %H:%M:%S'`
+
+if [ "$#" -ne 1 ]; then
+  msg="[ERROR]: $basefile failed to receive enough args"
+  echo "$msg"
+  echo "$msg" >> $logfile
+  exit 1
+fi
+
+function setup-logging(){
+  scope="setup-logging"
+  info_base="[$timestamp INFO]: $basefile::$scope"
+
+  echo "$info_base started" >> $logfile
+
+  echo "$info_base removing old logs" >> $logfile
+
+  rm -f $logfile
+
+  echo "$info_base ended" >> $logfile
+
+  echo "================" >> $logfile
+}
+
+function root-check(){
+  scope="root-check"
+  info_base="[$timestamp INFO]: $basefile::$scope"
+
+  echo "$info_base started" >> $logfile
+
+  #Make sure the script is running as root.
+  if [ "$UID" -ne "0" ]; then
+    echo "[$timestamp ERROR]: $basefile::$scope you must be root to run $0" >> $logfile
+    echo "==================" >> $logfile
+    echo "You must be root to run $0. Try the following"
+    echo "sudo $0"
+    exit 1
+  fi
+
+  echo "$info_base ended" >> $logfile
+  echo "================" >> $logfile
+}
+
+function docker-machine-check() {
+  scope="docker-machine-check"
+  info_base="[$timestamp INFO]: $basefile::$scope"
+  cmd=`whereis docker-machine`
+
+  echo "$info_base started" >> $logfile
+
+  if [ -z "$cmd" ]; then
+    echo "$info_base docker-machine not installed"
+    echo "$info_base docker-machine not installed" >> $logfile
+
+    echo "$info_base installing docker-machine"
+    echo "$info_base installing docker-machine" >> $logfile
+
+    ./docker-machine-install.sh
+  fi
+
+  echo "$info_base ended" >> $logfile
+  echo "================" >> $logfile
+
+}
+
+function usage() {
+    echo ""
+    echo "Usage: "
+    echo ""
+    echo "-u: start."
+    echo "-d: tear down."
+    echo "-h: Display this help and exit."
+    echo ""
+}
+function start-up(){
+
+    scope="start-up"
+    info_base="[$timestamp INFO]: $basefile::$scope"
+
+    echo "$info_base started" >> $logfile
+
+    echo "$info_base setting up swarm" >> $logfile
+
+    ./swarm-setup.sh
+
+    echo "$info_base starting swarm" >> $logfile
+
+    ./swarm-start.sh
+
+    echo "$info_base ended" >> $logfile
+
+    echo "================" >> $logfile
+}
+function tear-down(){
+
+    scope="tear-down"
+    info_base="[$timestamp INFO]: $basefile::$scope"
+
+    echo "$info_base started" >> $logfile
+
+    echo "$info_base removing swarm" >> $logfile
+
+    ./swarm-remove.sh
+
+    echo "$info_base swarm removed" >> $logfile
+
+    echo "$info_base ended" >> $logfile
+
+    echo "================" >> $logfile
+}
+
+root-check
+docker-machine-check
+
+while getopts ":udh" opts; do
+  case $opts in
+    u)
+      setup-logging
+      start-up ;;
+    d)
+      tear-down ;;
+    h)
+      usage
+      exit 0 ;;
+    /?)
+      usage
+      exit 1 ;;
+  esac
+done
